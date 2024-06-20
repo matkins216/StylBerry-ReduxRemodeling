8b0c8665d7e87814c8e165f101008d1ad30bfde7
Matt <matt.atkins216@gmail.com>
2024-06-20 12:34:18 -0600

    seo 20th

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,14 +9,15 @@ const Hero = () => {
         alt="redux background"
         className="object-cover h-svh w-full top-0 left-0 "
         fetchPriority="high"
+        loading="eager"
         
       />
       <div className="flex bg-black/40 absolute left-0 w-full h-screen z-10 justify-center" />
       <div className="flex flex-col whitespace-normal gap-10 md:w-1/3 md:min-h-[600px] rounded-2xl items-center p-10 md:p-16 w-fit mx-5 justify-center bg-white absolute z-10">
-        <h3 className="text-2xl font-bold text-center text-black mx-5 ">
+        <h1 className="text-2xl font-bold text-center text-black mx-5 ">
           Denver Home Remodeling and Renovations
 
-        </h3>
+        </h1>
         <p className=" flex text-wrap text-sm font-medium md:text-md xl:text-lg mx-5 text-center text-black  w-fit">
           Redux Remodeling is your premier choice when it comes to top-notch home transformations in Denver, Boulder, and beyond.
           <br /><br />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,12 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AboutHome from '../components/AboutHome'
-import ReactGA from 'react-ga4'
 import Footer from '../components/Footer'
 import Line from '../components/Line'
 import Testimonials from '../components/Testimonials'
 
 const Home = () => {
-     ReactGA.send({
-          hitType: 'pageview',
-          page: "/",
-          title: "Home",
-  })
+
      return (
           <div className=' bg-white h-full'>
                <Hero />
