347b04308a5b65ea0100c0775548289c7f17f157
Matt <matt.atkins216@gmail.com>
2024-06-25 11:47:05 -0600

    some other work4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="relative flex h-screen w-screen items-center md:w-full md:self-center font-rale justify-center">
+    <div className="relative flex h-fit w-screen items-center md:w-full md:self-center font-rale justify-center">
       <img
         src="https://i.postimg.cc/RCN8gwHz/jason-briscoe-Glia-HAJ3-5-A-unsplash.jpg"
         alt="redux background"
-        className="object-cover h-screen w-full top-0 left-0 "
+        className="object-cover h-fit w-full top-0 left-0 "
         fetchPriority="high"
         loading="eager"
         
       />
-      <div className="flex bg-black/40 absolute left-0 w-full h-screen z-10 justify-center" />
+      <div className="flex bg-black/40 absolute left-0 w-full h-full z-10 justify-center" />
       <div className="flex flex-col whitespace-normal gap-10 md:w-1/3 md:min-h-[600px] rounded-2xl items-center p-10 md:p-16 w-fit mx-5 justify-center bg-white absolute z-10">
         <h1 className="text-2xl font-bold text-center text-black mx-5 ">
           Denver Home Remodeling and Renovations
