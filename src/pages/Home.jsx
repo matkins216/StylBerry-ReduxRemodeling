1cf9d3c1c35b387b4abc6c0e1c4dc6b7918f8eec
Matt <matt.atkins216@gmail.com>
2024-06-25 11:33:11 -0600

    some other work2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,19 @@ import AboutHome from '../components/AboutHome'
 import Footer from '../components/Footer'
 import Line from '../components/Line'
 import Testimonials from '../components/Testimonials'
+import { Helmet } from 'react-helmet'
 
 const Home = () => {
 
      return (
           <div className=' bg-white h-full'>
+               <Helmet>
+                    <title>Redux Remodeling - Colorado Home Remodeling</title>
+                    <meta name='description' content='Redux Remodeling is your premier kitchen, bathroom, and basement remodeling contractor on the Front Range.' />
+                    <meta name='keywords' content='remodeling, kitchen, bathroom, basement, remodel, front range, Denver, Arvada ' />
+
+               </Helmet>
+             
                <Hero />
                <AboutHome />
                <Line />
