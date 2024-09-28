370910c72bf19d88c87a4f957c7811857bc445c2
Matt <matt.atkins216@gmail.com>
2024-09-28 13:37:40 -0600

    helmet fix

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ServicesHero from '../components/ServicesHero'
 import Services from '../components/Services'
 import ReactGA from 'react-ga4'
+import { Helmet } from 'react-helmet'
 
 const ServicesPage = () => {
   ReactGA.send({
@@ -11,6 +12,17 @@ const ServicesPage = () => {
   })
   return (
      <>
+      <Helmet>
+        <title>Redux Remodeling - Denver Area Remodeling Services</title>
+        <meta
+          name="description"
+          content="Redux Remodeling is your premier kitchen, bathroom, and basement remodeling contractor in the Greater Denver Metro."
+        />
+        <meta
+          name="keywords"
+          content="Denver home remodeling, home remodeling denver, kitchen remodeling, bathroom remodeling, basement finishing and remodeling, remodel, front range home remodeling, Denver, Arvada "
+        />
+      </Helmet>
       <ServicesHero />
       <Services />
      </>
